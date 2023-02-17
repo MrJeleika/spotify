3a690996724c2b4ed5b8270fe1d830ef8b16aa13
MrJeleika <den932005@gmail.com>
2023-02-17 20:26:40 +0200

    bugfix

--- src/components/Profile/ProfileMyPlaylists/ProfileMyPlaylists.tsx
+++ src/components/Profile/ProfileMyPlaylists/ProfileMyPlaylists.tsx
@@ -9,16 +9,17 @@ export const ProfileMyPlaylists = () => {
     <div>
       <h1 className="title ml-2 lg:ml-4">Public playlists</h1>
       <div className="flex w-full overflow-hidden">
-        {playlists.items.map((playlist, i) =>
-          playlist.public ? (
-            <Card
-              i={i}
-              key={i}
-              link={`playlist/${playlist.id}`}
-              item={playlist}
-            ></Card>
-          ) : null
-        )}
+        {playlists.next &&
+          playlists.items.map((playlist, i) =>
+            playlist.public ? (
+              <Card
+                i={i}
+                key={i}
+                link={`playlist/${playlist.id}`}
+                item={playlist}
+              ></Card>
+            ) : null
+          )}
       </div>
     </div>
   )
